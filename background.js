05b4bc4f1233b2de8fc55580b225feb1a398fd15
bethanw10 <bethanw10@gmail.com>
2020-11-03 16:14:03 +0000

    Add rename all feature

--- background.js
+++ background.js
@@ -4,21 +4,26 @@
 var pattern = "https://www.chickensmoothie.com/accounts/viewgroup.php*";
 
 async function redirect(details) {	
-	var gettingItem = await browser.storage.local.get('pageSize');
-	return addQueryString(details, "pageSize", gettingItem.pageSize || '100');
+	var gettingItem = chrome.storage.local.get('pageSize', function(result) {
+		var result = addQueryString(details, "pageSize", result.pageSize || '100');
+		
+		if (result) {
+			chrome.tabs.update(details.tabId, {url: result.redirectUrl});
+		}
+	});
 }
 
 function addQueryString(details, key, value) {
 	var re = new RegExp(`\\?.*(${key}=[^&]+)`)
 	var queryString = details.url.match(re);
-  
+
 	if (queryString) {
 		if (queryString[1] != `${key}=${value}`) {
 			return {
 				redirectUrl: details.url.replace(queryString[1], `${key}=${value}`)
 			}
 		}
-	} else {	
+	} else {
 		var seperator = details.url.includes("?") ? "&" : "?";
 			
 		return {
@@ -28,12 +33,12 @@ function addQueryString(details, key, value) {
 }
 
 function handleClick() {
-  browser.runtime.openOptionsPage();
+  chrome.runtime.openOptionsPage();
 }
 
-browser.browserAction.onClicked.addListener(handleClick);
+chrome.browserAction.onClicked.addListener(handleClick);
 
-browser.webRequest.onBeforeRequest.addListener(
+chrome.webRequest.onBeforeRequest.addListener(
   redirect,
   {urls:[pattern], types:["main_frame"]},
   ["blocking"]
